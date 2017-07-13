b3164264d41635cfe56523534684f1ee55dbf510
ata83 <ata83@sfu.ca>
2017-07-13 00:14:40 -0700

    search bar funtionality implemented and movie details visible

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,27 @@
 class PagesController < ApplicationController
+	require "net/http"
+	require "json"
   def home
-    
+
+  end
+
+  def searchresults
+	@APIKey = "946f9f3572e6d4c2d52768418fa0c39c"
+	@searchBarKeyword = params[:search]
+	@reqPrimed = @searchBarKeyword.sub ' ', '%20'
+	@apiReq = 'https://api.themoviedb.org/3/search/multi?api_key=' + @APIKey + "&query=" + @reqPrimed
+	@APIresponse = JSON.parse(Net::HTTP.get(URI(@apiReq)))
+	@arr = []
+	@arr= @APIresponse['results']
+  end
+
+  def movie
+  	@id = params[:id]
+  	@title = params[:title]
+  	@poster_path = params[:poster_path]
+  	@picture = "https://image.tmdb.org/t/p/w500" + @poster_path
+  	@overview = params[:overview]
+  	@release_date = params[:release_date]
+
   end
 end
