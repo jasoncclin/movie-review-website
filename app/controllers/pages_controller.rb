c6d7e607e723de53c4930fac69d85e5bffe99930
Simon Zhen <simonzhen@me.com>
2017-07-27 07:27:54 +0000

    Fixed some small stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,14 +8,14 @@ class PagesController < ApplicationController
   end
 
   def searchresults
-	@APIKey = "946f9f3572e6d4c2d52768418fa0c39c"
-	@searchBarKeyword = params[:search]
-	@reqPrimed = @searchBarKeyword.sub ' ', '%20'
-	@apiReq = 'https://api.themoviedb.org/3/search/multi?api_key=' + @APIKey + "&query=" + @reqPrimed
-	@APIresponse = JSON.parse(Net::HTTP.get(URI(@apiReq)))
-	@arr = []
-	@arr= @APIresponse['results']
-  @totRes = @APIresponse['total_results']
+  	@APIKey = "946f9f3572e6d4c2d52768418fa0c39c"
+  	@searchBarKeyword = params[:search]
+  	@reqPrimed = @searchBarKeyword.sub ' ', '%20'
+  	@apiReq = 'https://api.themoviedb.org/3/search/multi?api_key=' + @APIKey + "&query=" + @reqPrimed
+  	@APIresponse = JSON.parse(Net::HTTP.get(URI(@apiReq)))
+  	@arr = []
+  	@arr= @APIresponse['results']
+    @totRes = @APIresponse['total_results']
   end
 
   def movie
